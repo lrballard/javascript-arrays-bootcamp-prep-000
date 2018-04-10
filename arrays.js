2d736b970d9faa9fb4a366569282b3924ce4fd12
Lynn Ballard <lynn@pingsplash.com>
2018-04-10 02:54:08 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,61 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingeedient5 = "tomato"
+var ingredient6 = "garlic"
+
+var 
+grilledCheeseIngredients = 
+[
+  'bread',
+  'mild cheese',
+  'sharp cheese',
+  'butter',
+  'tomato',
+  'garlic'
+  ]
+  
+  var tomatoSauceIngredients
+  = [
+    'tomato',
+    'garlic',
+    'olive oil',
+    'basil',
+    'oregano'
+    ];
+    
+   
+   var primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37];
+    
+    var tvShows = ["game of thrones", "true detective", "the good wife", "empire"];
+    
+    var weirdGreeting = [
+      "he", 110, "w", 0, "r", 1, "d" ];
+      
+      var empty = []
+      
+      var evenNumbers = new
+      Array();
+      
+      var choclolateBars = ["snickers", "hundred grand", 
+      "kitkat", "skittles"];
+      
+      var superheroines = 
+      ["catwoman", "she-hulk", "mystique"];
+      
+      superheroines.push("wonder woman");
+      
+      var citites = ["New York", "San Francisco"];
+      
+      cities.unshift("Philadelphia");
+      
+      var citites = ["New York", "San Francisco"]
+      ;[...cities, "Philadelphia"];
+      
+     
+      cities
+      
+      
+      
+      
